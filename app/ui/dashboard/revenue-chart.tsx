475a0f78f7aec2c848d28cbdb9e3fe117bef483e
Takahiro <tkhr_621@docomo.ne.jp>
2024-06-06 19:12:59 +0900

    format

--- app/ui/dashboard/revenue-chart.tsx
+++ app/ui/dashboard/revenue-chart.tsx
@@ -1,7 +1,7 @@
-import { generateYAxis } from '@/app/lib/utils';
-import { CalendarIcon } from '@heroicons/react/24/outline';
-import { lusitana } from '@/app/ui/fonts';
-import { Revenue } from '@/app/lib/definitions';
+import type { Revenue } from '@/app/lib/definitions'
+import { generateYAxis } from '@/app/lib/utils'
+import { lusitana } from '@/app/ui/fonts'
+import { CalendarIcon } from '@heroicons/react/24/outline'
 
 // This component is representational only.
 // For data visualization UI, check out:
@@ -10,11 +10,11 @@ import { Revenue } from '@/app/lib/definitions';
 // https://airbnb.io/visx/
 
 export default async function RevenueChart({
-  revenue,
+  revenue
 }: {
-  revenue: Revenue[];
+  revenue: Revenue[]
 }) {
-  const chartHeight = 350;
+  const chartHeight = 350
   // NOTE: comment in this code when you get to this point in the course
 
   // const { yAxisLabels, topLabel } = generateYAxis(revenue);
@@ -61,5 +61,5 @@ export default async function RevenueChart({
         </div>
       </div> */}
     </div>
-  );
+  )
 }
